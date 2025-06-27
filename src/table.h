a99edc442884506d7f483f92f4547c32390228f2
kernelfucker <dikodut@protonmail.com>
2025-06-27 11:04:55 -0400

    confession

---
+++ src/table.h
@@ -0,0 +1,23 @@
+#ifndef table_h
+#define table_h
+
+#define version "0.1"
+
+typedef struct{
+	char source_ip[16];
+	char dest_ip[16];
+	unsigned short source_port;
+	unsigned short dest_port;
+	char protocol[8];
+	unsigned int bytes;
+	char timestamp[20];
+	char data[256];
+} Information;
+
+void start_analysis(const char *in, int tapping);
+void stop_analysis();
+int is_tor_active();
+void help();
+void show_version();
+
+#endif
